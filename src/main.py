b24008325dbff2120632b8b949dcd4884a54fba6
Amir01 <iran.volleyball79@gmail.com>
2021-07-12 23:47:58 +0430

    change: Add main loop and handle user commands

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from mysql.connector import connect, Error
+from prettytable import PrettyTable
 from query import *
+import functions
 import logging
+import getpass
 
 def config_logger():
     logger = logging.getLogger(__name__)
@@ -80,7 +83,8 @@ def init_db(connection, cursor, logger):
         connection.commit()
         logger.info('[init_db] Database successfully initialized.')
     except Error as e:
-        logger.exception('[init_db] An error occurred in init_db.')
+        logger.warning(f'[init_db] {cursor.fetchwarnings()}')
+        logger.warning('[init_db] Database has already been initialized.')
 
 
 if __name__ == '__main__':
@@ -99,5 +103,189 @@ if __name__ == '__main__':
             with connection.cursor() as cursor:
                 logger.info('initialize db.')
                 init_db(connection, cursor, logger)
+                
+                print('Welcome To Twitter\n')
+                
+                interactive = True
+                while interactive:
+
+                    ps = '\n>>>'
+                    current_user = functions.get_current_user(logger, cursor)
+                    if current_user:
+                        ps = f'\n[ {current_user} ] >>> '
+                    
+                    command = input(ps)
+
+                    if command.lower() in ('q', 'quit'):
+                        logger.info('shutting down program.')
+                        print('Have a Good day, dude. :)\nRemember us.\nTwitter')
+                        interactive = False
+                        continue
+
+                    com, *args = command.split()
+
+                    if com.lower() == 'signup':
+                        """
+                        SignUp command
+                        You can use this command to create your first account and enjoy a lot of cool tweets which are post everyday
+
+                        inputs:
+                            username    -> string at most with 20 characters which must be unique
+                            firstname   -> string at most with 20 characters
+                            lastname    -> string at most with 20 characters
+                            password    -> password of your account (please set a powerful password and do not use frequent passwords like 123456)
+                            biography   -> string at most with 64 characters, a text that describe you as good as possible
+                            birthdate   -> date with form %Y-%m-%d like 2020-03-09
+                        """
+
+                        if len(args) != 6:
+
+                            args = list()
+                            args.append(input('username > '))
+                            args.append(input('firstname > '))
+                            args.append(input('lastname > '))
+                            args.append(getpass.getpass(prompt='password (not shown)> '))
+                            args.append(input('biography > '))
+                            args.append(input('birthdate (like 2020-03-09) > '))
+
+                        functions.SignUp(logger, cursor, *args)
+
+                    elif com.lower() == 'login':
+                        """
+                        Login command
+                        You can use this command to login to your account and start using this fabulous app.
+
+                        inputs:
+                            username    -> string at most with 20 characters
+                            password    -> password of your account
+                        """
+
+                        if len(args) != 2:
+
+                            args = list()
+                            args.append(input('username > '))
+                            args.append(getpass.getpass(prompt='password (not shown) > '))
+
+                        functions.Login(logger, cursor, *args)
+
+                    elif com.lower() == 'history':
+                        """
+                        History command
+                        You can find last login of your self and other users of Twitter with this powerful command.
+
+                        inputs:
+                            username    -> string at most with 20 characters
+                        """
+                        
+                        if len(args) != 1:
+
+                            args = list()
+                            args.append(input('username > '))
+
+                        user_login_table = PrettyTable()
+                        user_login_table.field_names = ['Last Login']
+                        data = functions.UserLoginHistory(logger, cursor, *args)
+                        if data:
+                            user_login_table.add_rows(data)
+                            print(f'\t[{args[0]}]')
+                            print(user_login_table)
+
+
+                    elif com.lower() == 'follow':
+                        """
+                        Follow command
+                        You can use this command to follow users has registered in Twitter to follow their activities
+                        and their tweets.
+
+                        inputs:
+                            username    -> string at most with 20 characters
+                        """
+                        
+                        if len(args) != 1:
+
+                            args = list()
+                            args.append(input('username > '))
+
+                        functions.FollowUser(logger, cursor, *args)
+
+                    elif com.lower() == 'unfollow':
+                        """
+                        Unfollow command
+                        You can unfollow users that you have already followed
+
+                        inputs:
+                            username    -> string at most with 20 characters
+                        """
+                        
+                        if len(args) != 1:
+
+                            args = list()
+                            args.append(input('username > '))
+
+                        functions.UnfollowUser(logger, cursor, *args)
+
+                    elif com.lower() == 'ban':
+                        """
+                        Ban command
+                        You can ban users who seems have ironic tweets or something worse than we can think
+
+                        inputs:
+                            username    -> string at most with 20 characters
+                        """
+                        
+                        if len(args) != 1:
+
+                            args = list()
+                            args.append(input('username > '))
+
+                        functions.AddBan(logger, cursor, *args)
+
+                    elif com.lower() == 'unban':
+                        """
+                        Unban command
+                        You can unban each user who has been banned by you
+
+                        inputs:
+                            username    -> string at most with 20 characters
+                        """
+                        
+                        if len(args) != 1:
+
+                            args = list()
+                            args.append(input('username > '))
+
+                        functions.RemoveBan(logger, cursor, *args)
+
+                    elif com.lower() == 'tweet':
+                        pass
+                    elif com.lower() == 'liketweet':
+                        pass
+                    elif com.lower() == 'tweetlikesnum':
+                        pass
+                    elif com.lower() == 'likers':
+                        pass
+                    elif com.lower() == 'comment':
+                        pass
+                    elif com.lower() == 'commentsof':
+                        pass
+                    elif com.lower() == 'tweetsof':
+                        pass
+                    elif com.lower() == 'hashtagtweets':
+                        pass
+                    elif com.lower() == 'populartweets':
+                        pass
+                    elif com.lower() == 'followingstweets':
+                        pass
+                    elif com.lower() == 'mytweet':
+                        pass
+                    elif com.lower() == 'message':
+                        pass
+                    elif com.lower() == 'messageinbox':
+                        pass
+                    elif com.lower() == 'messagesfrom':
+                        pass
+
+                    connection.commit()
     except Error as e:
         logger.exception('An error occurred in database initialization')
+
